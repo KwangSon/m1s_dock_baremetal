8a0882ac4150fb7b7c1aba9df7f850e62e223322
Kwanghoon Son <kwangson@yahoo.com>
2023-01-21 19:51:13 +0900

    cleanup dependancy
    Signed-off-by: Kwanghoon Son <kwangson@yahoo.com>

--- sdk_intf_lib.cmake
+++ sdk_intf_lib.cmake
@@ -13,30 +13,30 @@ SET(MABI "ilp32e")
 endif()
 
 
-target_link_options(sdk_intf_lib INTERFACE
+target_link_options(sdk_intf_lib PUBLIC
 -Wl,--gc-sections -march=${MARCH} -mabi=${MABI} -mtune=${MCPU}
 )
 
-target_compile_options(sdk_intf_lib INTERFACE -O2 -march=${MARCH} -mabi=${MABI} -mtune=${MCPU})
+target_compile_options(sdk_intf_lib PUBLIC -O2 -march=${MARCH} -mabi=${MABI} -mtune=${MCPU})
 
 string(TOUPPER ${CHIP} CHIPNAME)
-target_compile_definitions(sdk_intf_lib INTERFACE -D${CHIPNAME})
+target_compile_definitions(sdk_intf_lib PUBLIC -D${CHIPNAME})
 if(CPU_ID)
 string(TOUPPER ${CPU_ID} CPU_ID_NAME)
-target_compile_definitions(sdk_intf_lib INTERFACE -DCPU_${CPU_ID_NAME})
+target_compile_definitions(sdk_intf_lib PUBLIC -DCPU_${CPU_ID_NAME})
 endif()
-target_compile_definitions(sdk_intf_lib INTERFACE -DARCH_RISCV -DBFLB_USE_HAL_DRIVER)
-target_compile_definitions(sdk_intf_lib INTERFACE -DCONFIG_IRQ_NUM=80)
+target_compile_definitions(sdk_intf_lib PUBLIC -DARCH_RISCV -DBFLB_USE_HAL_DRIVER)
+target_compile_definitions(sdk_intf_lib PUBLIC -DCONFIG_IRQ_NUM=80)
 
-target_include_directories(sdk_intf_lib INTERFACE utils/mmheap)
-target_include_directories(sdk_intf_lib INTERFACE utils/soft_crc)
-target_include_directories(sdk_intf_lib INTERFACE drivers/lhal/include)
-target_include_directories(sdk_intf_lib INTERFACE drivers/lhal/include/arch)
-target_include_directories(sdk_intf_lib INTERFACE drivers/lhal/include/arch/risc-v/t-head)
-target_include_directories(sdk_intf_lib INTERFACE drivers/lhal/include/arch/risc-v/t-head/Core/Include)
-target_include_directories(sdk_intf_lib INTERFACE drivers/lhal/config/${CHIP})
+target_include_directories(sdk_intf_lib PUBLIC utils/mmheap)
+target_include_directories(sdk_intf_lib PUBLIC utils/soft_crc)
+target_include_directories(sdk_intf_lib PUBLIC drivers/lhal/include)
+target_include_directories(sdk_intf_lib PUBLIC drivers/lhal/include/arch)
+target_include_directories(sdk_intf_lib PUBLIC drivers/lhal/include/arch/risc-v/t-head)
+target_include_directories(sdk_intf_lib PUBLIC drivers/lhal/include/arch/risc-v/t-head/Core/Include)
+target_include_directories(sdk_intf_lib PUBLIC drivers/lhal/config/${CHIP})
 
-target_include_directories(sdk_intf_lib INTERFACE 
+target_include_directories(sdk_intf_lib PUBLIC 
 drivers/soc/bl808/bl808_std/include
 drivers/soc/bl808/bl808_std/include/hardware
 )
